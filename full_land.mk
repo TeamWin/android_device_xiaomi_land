cce846f9f3f6647ff725c8e9691626d86f5c76c9
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2017-01-03 18:05:47 +0530

    land: Initial Commit
    Signed-off-by: Darshan Manjrekar <darshanmanjrekar1205@gmail.com>

---
+++ full_land.mk
@@ -0,0 +1,25 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+# Fstab
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/fstab.qcom:root/fstab.qcom
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := land
+PRODUCT_NAME := full_land
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 3S
+PRODUCT_MANUFACTURER := Xiaomi
---
+++ omni_land.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+$(call inherit-product, device/xiaomi/land/full_land.mk)
+
+$(call inherit-product, vendor/omni/config/common.mk)
+
+PRODUCT_NAME := omni_land
