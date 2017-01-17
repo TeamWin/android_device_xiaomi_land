f042e543ae14e90a62f8ed2f452d284cd0cefd43
Hriday Sharma <hridaysharma42@gmail.com>
2017-01-17 17:44:26 +0000

    land: kernel: clean up
    Signed-off-by: Hriday Sharma <hridaysharma42@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,7 +24,8 @@ TARGET_BOOTLOADER_BOARD_NAME := msm8937
 TARGET_NO_BOOTLOADER := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000
+BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
@@ -35,10 +36,9 @@ TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8937
-TARGET_KERNEL_CONFIG := lineageos_land_defconfig
+TARGET_KERNEL_CONFIG := land_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/kernel
+BOARD_DTBTOOL_ARGS := -2
 
 # Init
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
