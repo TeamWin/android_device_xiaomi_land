f0672ef39048800ec3e9196c17033baee3b74fbc
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2018-09-30 01:22:55 +0530

    land: Update
    * Kernel updated to Pie
    * Add gatekeeper & keymaster
    
    Change-Id: I645345e81b8c628a053caa8fcc7db7d37f3d8c70

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,7 +44,7 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x01000000
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/recovery/kernel
+TARGET_PREBUILT_KERNEL := device/xiaomi/land/prebuilt/Image.gz-dtb
 
 # Encryption
 TARGET_HW_DISK_ENCRYPTION := true
